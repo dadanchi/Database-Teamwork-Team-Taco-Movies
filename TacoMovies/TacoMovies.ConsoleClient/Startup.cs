2116e93ad6eb9aa6efe91288564303c18f9d927a
lazarlyutakov <lazarlyutakov@gmail.com>
2017-06-03 15:17:40 +0300

    adjust app.config and initiate DB in SQL server

---
+++ TacoMovies/TacoMovies.ConsoleClient/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TacoMovies.Models;
+
+namespace TacoMovies.ConsoleClient
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            var genre = new Genre();
+            genre.Name = "kur";
+            Console.WriteLine(genre.Name);
+        }
+    }
+}
--- TacoMovies/TacoMovies.Data/MoviesDbContext.cs
+++ TacoMovies/TacoMovies.Data/MoviesDbContext.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TacoMovies.Models;
 
 namespace TacoMovies.Data
 {
-    public class MoviesDbContext
+    public class MoviesDbContext : DbContext
     {
+        public MoviesDbContext() : base("MoviesDB")
+        {
+        }
+
+        public virtual IDbSet<User> Users { get; set; }
+
+        public virtual IDbSet<Artist> Artists { get; set; }
+
+        public virtual IDbSet<Movie> Movies { get; set; }
+
+        public virtual IDbSet<Genre> Genres { get; set; }
+
+        public virtual IDbSet<Award> Awards { get; set; }
+
     }
 }
